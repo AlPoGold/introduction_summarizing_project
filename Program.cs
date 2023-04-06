12287cc48ab2e9f2982fd742b8b2bf080df7ea88
Polina Kolesnikova <polina@example.com>
2023-04-06 14:19:59 +0400

    filling readme and adding image of algorithm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please, enter words with spaces between: ");
+string[] words = Console.ReadLine()!.Split(" ");
+int size = words.Length;
+int count = 0;
+for(int i =0; i<size; i++)
+{
+    if (words[i].Length<=3)
+    {
+        count++;
+    } 
+}
+string[] wordsWithThreeChars = new string[count];
+int index = 0;
+foreach (string word in words)
+{
+    if(word.Length<=3)
+    {
+        wordsWithThreeChars[index] = word;
+        index++;
+
+    }
+}
+
+foreach(string word in wordsWithThreeChars) Console.WriteLine(word);
